65ae61dbe178a3f2f62c3a1c897a003d776cb1ca
Teeranats <tnatt.61@gmail.com>
2023-01-17 14:52:29 +0700

    create components

--- angular-test-1/src/app/app.module.ts
+++ angular-test-1/src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FilingTypeComponent } from './components/filing-type/filing-type.component';
+import { MonthComponent } from './components/month/month.component';
+import { SaleAmountComponent } from './components/sale-amount/sale-amount.component';
+import { TaxAmountComponent } from './components/tax-amount/tax-amount.component';
+import { SurchargeComponent } from './components/surcharge/surcharge.component';
+import { PenaltyComponent } from './components/penalty/penalty.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FilingTypeComponent,
+    MonthComponent,
+    SaleAmountComponent,
+    TaxAmountComponent,
+    SurchargeComponent,
+    PenaltyComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
